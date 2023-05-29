b5beee8f0cbf87662c641e81a45e59a926700b4a
technero <nermeen.hamed86@gmail.com>
2023-05-29 13:23:24 -0700

    fetch the employee data

--- myscript.js
+++ myscript.js
@@ -1,7 +1,41 @@
 // global variables
-let employee = [];
+let employees = [];
 const urlAPI = 'https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US';
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+
+// fetch data from API
+fetch(urlAPI)
+    .then(res => res.json())
+    .then(res => res.results)
+    .then(displayEmployees)
+    .catch(err => console.log(err))
+
+
+function displayEmployees(employeeData) {
+    employees = employeeData;
+
+    // store the employee and creat HTML markup
+
+    let employeeHTML = "";
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+        // template literals 
+        employeeHTML += `
+   <div class= "card" data-index="${index}">
+   <img class= "avatar" src="${picture.large}"/> 
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    </div>
+  </div>
+   `
+    });
+    gridContainer.innerHTML = employeeHTML;
+}
