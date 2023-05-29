5f4caf6d7dc3e7e34e06e6072c2743f8898167ed
technero <nermeen.hamed86@gmail.com>
2023-05-29 12:38:40 -0700

    setup js file and set the global variables

---
+++ myscript.js
@@ -0,0 +1,7 @@
+// global variables
+let employee = [];
+const urlAPI = 'https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US';
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
