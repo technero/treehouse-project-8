683badcee5e5b5c97d4d774e1dac7dde56c94860
technero <nermeen.hamed86@gmail.com>
2023-05-30 14:14:11 -0700

    finished javascript

--- myscript.js
+++ myscript.js
@@ -39,3 +39,39 @@ function displayEmployees(employeeData) {
     });
     gridContainer.innerHTML = employeeHTML;
 }
+
+function displayModal(index) {
+
+    let { name, dob, phone, email, location: { city, street, state, postcode }, picture } = employees[index];
+
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+<img class= "avatar" src= "${picture.large}"/> 
+<div class="text-container">
+<h2 class="name">${name.first} ${name.last}</h2>
+<p class="email">${email}</p>
+<p class="address">${city}</p>
+<hr/>
+<p>${phone}</p>
+<p class="address">${street}, ${state} ${postcode}</p>
+<p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+</div>
+`;
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
+
+gridContainer.addEventListener('click', e => {
+
+    if (e.target !== gridContainer) {
+
+        const card = e.target.closest(".card");
+        const index = card.getAttribute("data-index");
+        displayModal(index);
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
